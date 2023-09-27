87682b1e3f607b6eed42ff65a9e563294157e441
Zachary Lyda <Oth1zlyda1993@gmail.com>
2023-09-27 13:47:35 -0400

    Put in set up to created readme with prompts based on example given in activity 20.

--- index.js
+++ index.js
@@ -11,3 +11,56 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is your name?',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'What languages do you know?',
+            name: 'language',
+        },
+        {
+            type: 'input',
+            message: 'What is your preferred method of communication?',
+            name: 'preferred',
+        },
+    ])
+    .then((answers) => {
+        const { name, language, preferred } = answers;
+
+        fs.appendFile('response.txt', `Name: ${name}\nLanguages: ${language}\nPreferred Language: ${preferred}\n\n`, (err) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log('Responses logged!');
+            }
+        });
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+
+// this creates the whole page below and place this within the function 
+
+  .then((response) => {
+        const fileData = `
+    This is the username: ${response.name}
+    
+    These are the languages: ${response.language}
+    
+    This is the communication: ${response.preferred}
+    `
+    });
+fs.writeFile('result.html', fileData, (err) =>
+  );
+
+  //use writefile to make one file and replace it each time it is run in the terminal
