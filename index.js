128f867eefe8ab355a5d40faf46bea81eb7bfd8f
Zachary Lyda <Oth1zlyda1993@gmail.com>
2023-09-27 14:54:03 -0400

    got everything set up to work as intended now to test for any errors.

--- index.js
+++ index.js
@@ -1,66 +1,117 @@
-// TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
-
-
 const inquirer = require('inquirer');
 const fs = require('fs');
+const licenseOptions = [
+    {
+      name: 'MIT License',
+      value: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+    },
+    {
+      name: 'Apache License 2.0',
+      value: '[![License: Apache License 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+    },
+    {
+      name: 'GNU General Public License (GPL)',
+      value: '[![License: GNU General Public License (GPL)](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://opensource.org/licenses/GPL-3.0)',
+    },
+    {
+      name: 'BSD License',
+      value: '[![License: BSD License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)',
+    },
+  ];
 
 inquirer
     .prompt([
         {
             type: 'input',
-            message: 'What is your name?',
-            name: 'name',
+            message: 'What is the title of your repositiory?',
+            name: 'title',
         },
         {
             type: 'input',
-            message: 'What languages do you know?',
-            name: 'language',
+            message: 'How would you describe the repository?',
+            name: 'description',
         },
         {
             type: 'input',
-            message: 'What is your preferred method of communication?',
-            name: 'preferred',
+            message: 'How would you use the application?',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'How would you install it?',
+            name: 'install',
+        },
+        {
+            type: 'input',
+            message: 'What are the contribution guideline?',
+            name: 'contGuide',
+        },
+        {
+            type: 'input',
+            message: 'How would you test it?',
+            name: 'test',
+        },
+        {
+            type: 'list',
+            message: 'Select a license for your application:',
+            name: 'license',
+            choices: licenseOptions,
+          },
+        {
+            type: 'input',
+            message: 'What is your GitHub Username?',
+            name: 'username',
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email',
         },
     ])
-    .then((answers) => {
-        const { name, language, preferred } = answers;
+    .then((response) => {
+        const readMe = `
+        # ${title}                           ${answer.license}
 
-        fs.appendFile('response.txt', `Name: ${name}\nLanguages: ${language}\nPreferred Language: ${preferred}\n\n`, (err) => {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log('Responses logged!');
-            }
-        });
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
+        # Description
 
-// this creates the whole page below and place this within the function 
+        ## ${description}
 
-  .then((response) => {
-        const fileData = `
-    This is the username: ${response.name}
-    
-    These are the languages: ${response.language}
-    
-    This is the communication: ${response.preferred}
+        # Table of Contents
+
+        - [Installation](#installation)
+        - [Usage](#usage)
+        - [Contribution](#contribution)
+        - [Test Instructions](#test instructions)
+        - [Licenses](#licenses)
+        - [Questions](#questions)
+
+        # Installation
+
+        ## ${install}
+
+        # Usage
+
+        ## ${usage}
+
+        # Contribution
+
+        ## ${contGuide}
+
+        # Test Instructions
+
+        ## ${test}
+
+        # Licenses
+
+        ## ${license}
+
+        # Questions
+
+        ## GitHub Repository: https://github.com/${username}
+
+        ## If you need to contact me you can reach me at ${email}.
     `
     });
-fs.writeFile('result.html', fileData, (err) =>
-  );
+    fs.writeFile('${title}.md', readMe, (err));
 
-  //use writefile to make one file and replace it each time it is run in the terminal
+    fs.writeFileSync('${title}.md', readmeContent);
